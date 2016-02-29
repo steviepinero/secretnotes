24786019d0d773385e68722887e0464024954244
Stevie <steviepinero@gmail.com>
2016-02-29 11:36:26 -0500

    can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    @user = User.
+            find_by(username: params[:username]).
+            try(:authenticate, params[:password])
+
+    return render action: 'new' unless @user
+
+    #logged in
+    session[:user_id] = @user.id
+    redirect_to_notes_path
+  end
 end
